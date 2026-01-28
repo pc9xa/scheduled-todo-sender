85a0c90b3063a97d6330880476eabae11e8eb262
pc9xa <pygarcs@gmail.com>
2026-01-28 22:59:33 +0800

    - Change file handling for env file to use absolute path

--- sender.py
+++ sender.py
@@ -7,9 +7,10 @@ from datetime import datetime, timezone, timedelta
 # - CONSTANTS -----------------------------------------------------------------
 BASEDIR = Path(__file__).resolve().parent
 tasks_path = BASEDIR / "tasks.json"
+env_path =  BASEDIR / ".env"
 
 # - ENVIRONMENT VARIABLES -----------------------------------------------------
-load_dotenv()
+load_dotenv(dotenv_path=env_path)
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCT_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 WA_VIRTUAL_NUMBER = os.getenv("WHATSAPP_VIRTUAL_NUMBER")
