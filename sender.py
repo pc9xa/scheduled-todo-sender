5f9f521563797b80a25b3b2cfbebd521008dcd3e
pc9xa <pygarcs@gmail.com>
2026-02-09 12:21:53 +0800

    - Remove .env dependency (rely on Github secrets)

--- sender.py
+++ sender.py
@@ -1,21 +1,19 @@
 import os
-from pathlib import Path
-from dotenv import load_dotenv
 from twilio.rest import Client
 from upstash_redis import Redis
 from datetime import datetime, timezone, timedelta
 
 # - CONSTANTS -----------------------------------------------------------------
 TASK_DB = "Task_list"
-BASEDIR = Path(__file__).resolve().parent
-env_path =  BASEDIR / ".env"
 
 # - ENVIRONMENT VARIABLES -----------------------------------------------------
-load_dotenv(dotenv_path=env_path)
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCT_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 WA_VIRTUAL_NUMBER = os.getenv("WHATSAPP_VIRTUAL_NUMBER")
 MY_NUMBER = os.getenv("MY_PHONE_NUMBER")
+
+UPSTASH_URL = os.environ["UPSTASH_URL"]
+UPSTASH_TOKEN = os.environ["UPSTASH_TOKEN"]
 
 # - MESSAGE CREATION ----------------------------------------------------------
 # - Date & time
@@ -25,8 +23,8 @@ header = f"{today.strftime('%B %d, %Y')} - {today.strftime('%A')}:\n"
 
 # - Redis connection
 redis = Redis(
-    url=os.getenv("UPSTASH_URL"),
-    token=os.getenv("UPSTASH_TOKEN")
+    url=UPSTASH_URL,
+    token=UPSTASH_TOKEN
 )
 to_do_list = []
 for task in redis.lrange(TASK_DB, 0, -1):
